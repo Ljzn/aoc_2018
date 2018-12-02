8425d87fe8ce2860e47a835a81fd393734c2ebed
LJZN <rubyminer@foxmail.com>
2018-12-02 15:08:58 +0800

    day2

---
+++ lib/day2.ex
@@ -0,0 +1,298 @@
+defmodule Day2 do
+  @input ~w(wxlnjevbfozadyiqpuzkrhstkg
+  wxlsjivbfodamyiqpuzcxhstkg
+  wxlnjevbfouammnqpuzcrhstkg
+  wxlnjevbfobwmyiqpuzprhstkg
+  wxlnjefbfodamyiqpuzcnustkg
+  wxlnjevbfodakyyupuzcrhstkg
+  wxlnjejbfodafynqpuzcrhstkg
+  wxlnjevbfodomyifptzcrhstkg
+  wxlnjevbfodamyiwcuzcrhstkz
+  wxlnjevbfofamyiqpuznrhstxg
+  wxlnjevbfodamyiqbupcrxstkg
+  wxjnjevbfodxmyeqpuzcrhstkg
+  xwlnjevbfosamyiqpuzcrhstkg
+  fxlnjevbfodrmyiqpuzcrbstkg
+  wxlnjevpfodamyiqquzzrhstkg
+  wwlnjenbfodawyiqpuzcrhstkg
+  wxrnjevbfodamyiqpuzlrhstrg
+  welnjeqbfodkmyiqpuzcrhstkg
+  walnjfvbfodamyiqpuzcrhwtkg
+  wdlnjevbfodamriqpuzjrhstkg
+  wxlnjevbfodmnyizpuzcrhstkg
+  wxlnjevbfodgmyiqpuxcrjstkg
+  wxlnjevbfkdamyiqpudcrestkg
+  wxlyjevbfodamyiqpuzcehstkj
+  wxlnjevamodamyiqpuzcrhatkg
+  fxlnqevsfodamyiqpuzcrhstkg
+  wqlnjevbfodanyiqvuzcrhstkg
+  wxlnjevbfoiamyzqpuzcrnstkg
+  wxlnjevbfodamyiqpuacrhsjig
+  wxlnjuvbfodzmyvqpuzcrhstkg
+  kxlnjevbfolamviqpuzcrhstkg
+  wxlnjesbfldamyiqpuycrhstkg
+  nxltjevbfodgmyiqpuzcrhstkg
+  ojlnjevbfooamyiqpuzcrhstkg
+  wxlnjevbfodaffiqpugcrhstkg
+  cxlnievbfodamyuqpuzcrhstkg
+  wxlouevbjodamyiqpuzcrhstkg
+  wafnjevbfhdamyiqpuzcrhstkg
+  wxlnjevbfxdamrpqpuzcrhstkg
+  wxlnjepffodamyiqphzcrhstkg
+  wxlnkevbfohamciqpuzcrhstkg
+  wzlnmevdfodamyiqpuzcrhstkg
+  wxlzjtvbfodamyiqpuzcrhstkd
+  gxlnjehbfojamyiqpuzcrhstkg
+  wxlnjeoqfodamyiqprzcrhstkg
+  nxllvevbfodamyiqpuzcrhstkg
+  wxlnjevtfomamyiqpurcrhstkg
+  sxlnjevafodamyikpuzcrhstkg
+  wxlnjevbfodamyfqpuzcyhztkg
+  wxlnjevbfodamyiqpulnrhstkh
+  wxlnwevbfodumyiqpuzqrhstkg
+  wxldjevbfodamyiqpzzcrhstkk
+  jxlnjevbfodamyiqphzcrnstkg
+  fxlnjeibfodcmyiqpuzcrhstkg
+  wxlnjevufodamyiqpnzcrhstkk
+  wglnjevbfodamyiqpuzcesstkg
+  wxlvjevbdodamyiqpuzcrhstkc
+  wxlnjevbfodabyicpuzcrhstkl
+  wxlnjevbfodamyiqpizcrhstvt
+  wolnjevbfodawyiqiuzcrhstkg
+  wxlyjevbfodamyuqpxzcrhstkg
+  wxlijevbfodamyikpuzyrhstkg
+  wxennevbfodamyiqpuzcrtstkg
+  wxlnjevbyodamyuqpwzcrhstkg
+  wxlnjevbfoiomyiqpuzcrhsteg
+  wxlnjehbrodamyiqpuicrhstkg
+  xxlnjevufodamyiqbuzcrhstkg
+  wxlojevbfodamyiqpezcrhatkg
+  wxljjevbfolamuiqpuzcrhstkg
+  wxlnjevbfodamyiqruzcrhstpi
+  wxlnjevbfomamyiqjnzcrhstkg
+  wxlnjevbfodahyiqzuzcrhstpg
+  wxtnjevbfodamyiqpuzcrhsdrg
+  wxlnjevbfodamynrpuzcrhstkz
+  wxlqjevefqdamyiqpuzcrhstkg
+  wxlnjevbfmdamyiqnuzckhstkg
+  wxlnjevbfodajyiqprzcrjstkg
+  wxlnjqvbhodamyidpuzcrhstkg
+  wxlnjhvbfodamriqpuzcchstkg
+  wglnjevbfodamyiqpupfrhstkg
+  wulnjevdfodamyiqpuzcrhsteg
+  vxlojevbfodamyiqpuzcrhstsg
+  wxlnjvvbfodamiiqpuzcrhttkg
+  wxlnjevbfodabyiqpuzzrhetkg
+  wxhnjevbfodamyiqpuwcrsstkg
+  wslzjbvbfodamyiqpuzcrhstkg
+  rxlnjevbfodhmyiqpupcrhstkg
+  wxlnjevbfhdamyiqpuvcrhskkg
+  wxlrjevbxodamyiqpuzcrhstag
+  wxlsbevbfodammiqpuzcrhstkg
+  wxlnjzvbfodemyiqpmzcrhstkg
+  wxlnoevbfodgmyiqpuzbrhstkg
+  wxlnjefbfodamyinpuzcrhwtkg
+  bxlnjevbfwdamyiqpuocrhstkg
+  cxlnjevbjodamyiqpuzcrhslkg
+  wflnjevbforemyiqpuzcrhstkg
+  wxlmjeoboodamyiqpuzcrhstkg
+  wxlnjevbfadaiyiqpuzcrhutkg
+  wxlnmevbfodamyyqpuzcrjstkg
+  wxlnjovbfodamyippjzcrhstkg
+  wxlnjmvbfodamyiqpszcrhsbkg
+  wxlnjeebfodamyicpuxcrhstkg
+  wxlnrehbfodamyiqpuzcrhytkg
+  wxlnjevbfogamyiqwurcrhstkg
+  wxlujevbnodamyiqpuzcrhstng
+  wxlnoenofodamyiqpuzcrhstkg
+  wxsnjevbfsdamyiqsuzcrhstkg
+  wxlnjevwfodamyiqpuzxrhqtkg
+  wxlnjevbnodamyiqxulcrhstkg
+  wxlijetpfodamyiqpuzcrhstkg
+  wxlnjzvbfidamyiqpuzcrbstkg
+  wxlnjevefodavyiqpuzcthstkg
+  wxlnjevbfozamyiqpurcrbstkg
+  wxlnjfvpfodamyiqpuzcrhntkg
+  wxlnjevbfvdamyiqvuzcrhqtkg
+  wilejevbfodamyilpuzcrhstkg
+  wxlnhevbfodamtiqpuzcrhstke
+  wxlwjevbfodahyiqppzcrhstkg
+  wxlnjevbfodamyuqpuzwrrstkg
+  xxsnjevbfodamyiqpuzcrhstkl
+  wglnjevbdodamyaqpuzcrhstkg
+  wxlnjefbwodamyiqpuzcrhsykg
+  wxwnjevbfodamyiqpuzcrhpckg
+  wxlnjuvbfidamyiqpuzczhstkg
+  wxlnzhybfodamyiqpuzcrhstkg
+  wxunjevufodamyiqpuzcrhspkg
+  wxunjevbfodcmyiqpuzcrhstcg
+  wxlnjevbfodhwyiqpuxcrhstkg
+  wxlnjevtfodasyiqpuzcrhstkb
+  wxlvjevbfqdamyiqprzcrhstkg
+  sxlnjevbfodamyiqplzcrhstkq
+  wxlnlevbfodamyiqpuzcrpstka
+  wxlnjevbfodaiyiqovzcrhstkg
+  wxlntevbfodamyiqpuzcrkntkg
+  wxlnjevbfodsmyiqpuzcrhstir
+  wxlnnevbfodaoyiqpuzmrhstkg
+  xxlnjevbfodamyinpnzcrhstkg
+  wxlnjedefodamyigpuzcrhstkg
+  wxlnxeabfodamyiqpnzcrhstkg
+  wxlnxevbfodpmtiqpuzcrhstkg
+  wxlnjevnfodamyiqpuzcuhqtkg
+  wxlnjevbfodakyiqluzcrhstmg
+  wxlnjevbaodamoiqpyzcrhstkg
+  wwlnjevbfoaajyiqpuzcrhstkg
+  wxlnjevbfedamyiqpuzcrhsang
+  wxlwjevbfodamyiqpuzcrmdtkg
+  wxlnjevbhodamyiqpmzxrhstkg
+  wxlnjevbfodamyiqpuzzrhwtkj
+  wxlnjevbfpdvmyiqpuzzrhstkg
+  wxlnjegcfodamyiqpxzcrhstkg
+  fxlnjevbfodamyiqpuzcrhstat
+  wxlnjevbfodcmybqpuzcrkstkg
+  wxlnjevbfodamiiqpuzrrhstxg
+  wxvnjevifodamdiqpuzcrhstkg
+  wxltjevbfodamyiqpuzmrhktkg
+  wxlnjevbfobaaygqpuzcrhstkg
+  wmlnjevbfodamyiqpuycrhsukg
+  wxlnjevboodamyiqpuzcrhuhkg
+  wxlnjevgfodaqyiqpuzcghstkg
+  wxlnjevjnodamyiqpuzcrhstke
+  wclnjevbfodamyiqpuncchstkg
+  wxlnjevbfndamyxqpuzcshstkg
+  rxldjevbfodamyiqpuvcrhstkg
+  wxlnwevbfodamywqpuzrrhstkg
+  ixlnjevbqodpmyiqpuzcrhstkg
+  wxlnjlvbfocamyiqpuzgrhstkg
+  wxlnjevffodamyiqnuzcrhutkg
+  wxlajevbfodamyiqpuccrhshkg
+  vwlnjevbfodamyiqpuzcrhstky
+  wxlajevbfodamyiqpuzcfhstkl
+  wxlnjevbfodamniqouzcrhstko
+  wxlnjevbfodamyiqpuzqrhsqka
+  wxlnjeybfodamyiqpuzclhsnkg
+  wxbnjlvbfoyamyiqpuzcrhstkg
+  wxbnjevbfodemyiqpuzcrhstkj
+  wxlnbefbfodamyiqpkzcrhstkg
+  wxlnjvvbyodamyitpuzcrhstkg
+  jxlnjevbfopamyiqpuzprhstkg
+  wxlnjevbfodaeyiupxzcrhstkg
+  wnlnjevbfodamyqqpuzcrhstcg
+  wxlxzuvbfodamyiqpuzcrhstkg
+  wxlnjevbcodymyiqpuzcrhstke
+  wxlnjezbfodamynqpuvcrhstkg
+  wxlnjevbfodamyxlpuzcyhstkg
+  wxlnjevbffdaiyiqpuzirhstkg
+  wxlnjevbfodymyiqwuzcrhstfg
+  wxlnzevbfodscyiqpuzcrhstkg
+  hxlnjevbfodamyawpuzcrhstkg
+  welnjevbfodamciqplzcrhstkg
+  wxlnjeqbfodawyiqpuzkrhstkg
+  wxlnjelbfodamviqpuzckhstkg
+  wxlneevjfodamyiqpuzcrhstkd
+  wxlnjevbfodamyaqpuytrhstkg
+  wxlnjpvyfodamyiqpuzcshstkg
+  wxlnjevbfodmbyiqpuzcrhstkp
+  wxlnjegbfodamdiqpuzcrhstdg
+  wmlnjevbfodamyiqpuecrhsukg
+  wxlnjevbfodamyiqpuocrhwtjg
+  jxfnwevbfodamyiqpuzcrhstkg
+  wxlnjevffodamyiqpurcrhstkd
+  wxlnjevbfofamyiqpuzcrhsmkt
+  wxlnjevbfodmmyiqpuzcrdsttg
+  axlnjevbfodgmyiqpuzerhstkg
+  wxtnjevbfodamyifpuwcrhstkg
+  wxlgjevbfodamyiqpuzrrhvtkg
+  wxlnjevbfouamyeqfuzcrhstkg
+  wxlnjevbfmxamyiqpuzcahstkg
+  wxlnjevffoxamyiqpuecrhstkg
+  wxlnyevbfodamyiqttzcrhstkg
+  bxlnjevbfodzmysqpuzcrhstkg
+  wxlnjevbfodayyiqpuzcrherkg
+  yxlnjevbfodayyiqpuzcrwstkg
+  wllnjevbfodambiqpuzurhstkg
+  wxlnjevbfsdamyiqpuqcrhstkh
+  wxlcjevbfodamyiqcuzcxhstkg
+  wxlnjevbfodayticpuzcrhstkg
+  wxltzevbfodamyiqpzzcrhstkg
+  wxlnjevbfodamgiqpuzcphstng
+  wxlnjevbfqdfmziqpuzcrhstkg
+  wxlnaevbfodamyiqpuzcfustkg
+  wxlnjevbfodamyxqxuzcrhstdg
+  wxlnjevkbodamyiqpufcrhstkg
+  whlnjevbfodauyiqputcrhstkg
+  wxlnjevbfodamyiephzcrhsnkg
+  wxlnjevbfodfmoiqpuzcrhstkf
+  wxlnjevbfodamyiqxuzaxhstkg
+  wxlnjevtfotamyiqpuzcrhsttg
+  wxlgjevbfodamyiqpuhcrostkg
+  dxlnjtvbfodamyiqpuzcshstkg
+  wxlfjevbfodumyiqppzcrhstkg
+  wxlnzevbfodamyiqpuzqrhstkx
+  wflnjevbfodamyiqpurcrhsthg
+  wxlnjevbfodzfyiqpuzcrjstkg
+  wxlnjevbfrdamviqpuzmrhstkg
+  wnlngevmfodamyiqpuzcrhstkg
+  walzjevbfodamyiqpuzcrhsjkg
+  wqlnjevbfodamyiqpuzcshslkg
+  wxlnjevkfodfmyiepuzcrhstkg
+  wxgnjehbfodamyhqpuzcrhstkg
+  wxlnjevbfodamyiqfuacrostkg
+  wxlnjexbfodamyiwpuzcrqstkg
+  wxlntevafodamyiqpuzcrhsnkg
+  wxvnjevbfodamyiqpuzcvistkg
+  mxlnjeebfodamyiqpuzcrhsgkg
+  wxlnjevyfodamyiqpuzcrhdtkf
+  wxlnjcvbfodamyicpuzcrhsckg
+  wxlnjekbfodlmyiqpuzcthstkg
+  wxlnjvvbfodamyiopuzcrhstqg
+  wxlnjevbsodamyiqpuhcrhstwg
+  wxxnjevufodamyiqruzcrhstkg)
+
+  def run do
+    tags =
+      @input
+      |> Stream.map(&count_letter(&1, %{}))
+      |> Stream.map(&tag/1)
+
+    two = Enum.count(tags, fn {t, _} -> t end)
+    three = Enum.count(tags, fn {_, t} -> t end)
+    two * three
+  end
+
+  defp count_letter(<<h, t::binary>>, counts) do
+    count_letter(t, Map.update(counts, h, 1, &(&1 + 1)))
+  end
+  defp count_letter(<<>>, counts) do
+    # %{"a" => 2, "b" => 3}
+    counts
+  end
+
+  defp tag(counts) do
+    is_two = Enum.any?(counts, fn {_, v} -> v == 2 end)
+    is_three = Enum.any?(counts, fn {_, v} -> v == 3 end)
+    {is_two, is_three}
+  end
+
+  def run2 do
+    {a, b, _} =
+      for x <- @input, y <- @input -- [x] do
+        {x, y, String.jaro_distance(x, y)}
+      end
+      |> Enum.reject(fn {_, _, j} -> j == 1 end)
+      |> Enum.max_by(fn {_, _, j} -> j end)
+
+    common_letters(a, b, [])
+  end
+
+  defp common_letters(<<h, x::binary>>, <<h, y::binary>>, c) do
+    common_letters(x, y, c ++ [h])
+  end
+  defp common_letters(<<_, x::binary>>, <<_, y::binary>>, c) do
+    common_letters(x, y, c)
+  end
+  defp common_letters(<<>>, <<>>, c) do
+    c
+  end
+end
